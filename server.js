b5bfb00dfcf8e7c5fe775ed2e29404cfd3300a3d
max0man0 <0naxman0@gmail.com>
2024-05-19 21:11:55 +0300

    query param support

--- server.js
+++ server.js
@@ -8,6 +8,44 @@ const db = 'https://backend-database-olz2xjbmza-uc.a.run.app'
 
 app.use(cors());
 
+app.delete('', async (req, res, next) => {
+  // Get the id as a query parameter
+  if (!req.query.id) {
+    res.status(400).send('Missing id parameter');
+    next();
+    return;
+  }
+  const id = req.query.id;
+  const url = `${db}/${id}`;
+  const options = {method: 'DELETE'}
+
+  let status = 0;
+  let content = '';
+  await fetch(url, options)
+    .then(async response => {
+      if (response.status === 204) {
+        status = 204
+        console.log(`Medicine with id ${id} deleted successfully`);
+      } else {
+        status = response.status
+        content = await response.json()
+      }
+    })
+    .catch(error => {
+      status = 500
+      content = 'Internal server error occurred'
+      console.error('Error:', error);
+    });
+
+    if (content === '') {
+      res.status(status).send();
+    } else {
+      res.status(status).send(content);
+    
+    }
+  next();
+});
+
 app.delete('/:id', async (req, res, next) => {
   const id = req.params.id;
   const url = `${db}/${id}`;
@@ -15,7 +53,6 @@ app.delete('/:id', async (req, res, next) => {
 
   let status = 0;
   let content = '';
-  console.log(`Before fetch to ${url}`);
   await fetch(url, options)
     .then(async response => {
       if (response.status === 204) {
