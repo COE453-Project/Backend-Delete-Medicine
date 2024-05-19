32ab6747face76717d59fd3b5642b6e12a780e1b
Mahmoud Alrimawi <129902545+M-Alrimawi@users.noreply.github.com>
2024-05-19 16:06:52 +0300

    Connected to DB service

--- server.js
+++ server.js
@@ -4,17 +4,29 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
+const db = 'https://backend-database-olz2xjbmza-uc.a.run.app'
 
 app.use(cors());
 
 app.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
+  const url = `${db}/${id}`;
+  const options = {method: 'DELETE'}
 
-    // TODO delete the item from the database
-    console.log(`Deleting item with id ${id}`);
-
-    res.status(204).send();
-    next();
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        res.status(500).send(`Medicine with id ${id} deleted successfully`);
+        console.log(`Medicine with id ${id} deleted successfully`);
+      } else {
+        res.status(response.status).send(`Failed to delete medicine ${id}`)
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      res.status(500).send('Internal server error occurred');
+    });
+  next();
 });
 
 app.use(logResponse);
